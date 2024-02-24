041ee8345548acd417a82508b71df1817474d21a
l-khenniche <khennicl@3il.fr>
2024-02-24 17:09:16 +0100

    Ajout du joueur et des déplacements

--- code/game.py
+++ code/game.py
@@ -1,6 +1,8 @@
 import pygame
 from screen import Screen
 from map import Map
+from entity import Entity
+from keylistener import KeyListener
 
 class Game:
 
@@ -8,9 +10,23 @@ class Game:
         self.running = True
         self.screen = Screen()
         self.map = Map(self.screen)
+        self.keylistener = KeyListener()
+        self.entity = Entity(self.keylistener)
+        self.map.add_player(self.entity)
 
 
     def run(self):
         while self.running:
+            self.handle_input()
             self.map.update()
             self.screen.update()
+
+    def handle_input(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            elif event.type == pygame.KEYDOWN:
+                self.keylistener.add_key(event.key)
+            elif event.type == pygame.KEYUP:
+                self.keylistener.remove_key(event.key)
+
