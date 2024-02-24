2eeaef47e63ed7ba9f3e03e43bbb3f7094e0f7e1
l-khenniche <khennicl@3il.fr>
2024-02-24 17:34:58 +0100

    correction image player avec un fond

--- code/entity.py
+++ code/entity.py
@@ -8,7 +8,7 @@ class Entity(pygame.sprite.Sprite):
     def __init__(self, keylistener: KeyListener):
         super().__init__()
         self.keylistener = keylistener
-        self.spritesheet = pygame.image.load("assets/sprite/hero_01_red_m_walk.png")
+        self.spritesheet = pygame.image.load("assets/sprite/hero_01_red_m_walk.png").convert_alpha()
         self.image = Tool.split_image(self.spritesheet, 0, 0, 24, 32)
         self.position = [0, 0]
         self.rect: pygame.Rect = pygame.Rect(0, 0, 16, 32)
